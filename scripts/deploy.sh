c39a304b3ec550a288655c868eb91770a175d4aa
DC <dcposch@dcpos.ch>
2018-01-15 00:52:41 -0500

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,3 @@
-tar -czvf - . | ssh datpedia.us-east-1.aws "cd /mnt/disk && tar -xzvf -"
+cd .. && tar -czvf - datpedia | ssh datpedia.us-east-1.aws "cd /mnt/disk && tar -xzvf -"
+cd datpedia
+echo "done"
