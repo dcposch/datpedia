7b42b67950f2b7dae1528d0ca57b01fd17455470
DC <dcposch@dcpos.ch>
2018-01-16 23:04:53 -0500

    zero: range request demo

--- zero/index.js
+++ zero/index.js
@@ -1,15 +1,28 @@
+main()
 
-const wikiDatAddr = '/wikipedia_en_simple_all_2017-01.zim'
+async function main () {
+  const wikiDatAddr = '/wikipedia_en_simple_all_2017-01.zim'
 
+  const headers = new Headers({
+    'Range': 'bytes=0-1023'
+  })
 
-const headers = new Headers({
-  'Range': 'bytes=0-1023'
-})
+  console.log('Fetching...')
+  const root = document.querySelector('#root')
 
-console.log('Fetching...')
-const root = document.querySelector('#root')
-fetch(wikiDatAddr, { headers })
-  .then(res => root.innerText = 'it worked! range request result: ' + res.blob())
-  .catch(err => console.error(err))
+  const res = await fetch(wikiDatAddr, { headers })
+  if (res.ok) {
+    const buf = await res.arrayBuffer()
+    root.innerText = 'it worked! range request result: ' + buf2hex(buf)
+  } else {
+    root.innerText = 'range request failed: ' + res.status
+  }
+}
 
+function byte2hex (x) {
+  return ('00' + x.toString(16)).slice(-2)
+}
 
+function buf2hex (buffer) {
+  return Array.prototype.map.call(new Uint8Array(buffer), byte2hex).join(' ')
+}
