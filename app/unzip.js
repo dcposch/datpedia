e35e59032392e4c1941a806bbfa0fe69e1e16a7c
DC <dcposch@dcpos.ch>
2018-01-19 16:38:53 -0500

    class-async, webpack hates it

--- app/unzip.js
+++ app/unzip.js
@@ -110,13 +110,14 @@ class ZipRandomAccessReader extends yauzl.RandomAccessReader {
   constructor (zipPath) {
     super()
     this._zipPath = zipPath
+    this._pagePromiseCache = []
   }
 
   _readStreamForRange (start, end) {
     const through = new stream.PassThrough()
 
     // Convert [start, end) to [start, end]
-    readBufsForRange(this._zipPath, start, end - 1)
+    this._readBufsForRange(start, end - 1)
       .then((pages) => {
         pages.forEach(page => through.write(page))
         through.end()
@@ -128,59 +129,56 @@ class ZipRandomAccessReader extends yauzl.RandomAccessReader {
   /* read (buffer, offset, length, position, callback) {
 
   } */
-}
 
-// TODO: LRU
-const _pagePromiseCache = []
-
-/**
- * Reads bit range [start, end], inclusive. Returns buffers to concat.
- */
-async function readBufsForRange (path, start, end) {
-  // Kick off any fetches not yet started
-  const pageStart = start >> PAGE_BITS
-  const pageEnd = end >> PAGE_BITS
-  for (let page = pageStart; page <= pageEnd; page++) {
-    let promise = _pagePromiseCache[page]
-    if (promise == null) {
-      promise = _pagePromiseCache[page] = readPage(path, page)
+  /**
+   * Reads bit range [start, end], inclusive. Returns buffers to concat.
+   */
+  async _readBufsForRange (start, end) {
+    // Kick off any fetches not yet started
+    const pageStart = start >> PAGE_BITS
+    const pageEnd = end >> PAGE_BITS
+    for (let page = pageStart; page <= pageEnd; page++) {
+      let promise = _pagePromiseCache[page]
+      if (promise == null) {
+        promise = _pagePromiseCache[page] = this._readPage(page)
+      }
     }
+
+    // Return buffers
+    const ret = new Array(pageEnd - pageStart + 1)
+    for (let page = pageStart; page <= pageEnd; page++) {
+      let buf = await _pagePromiseCache[page]
+      if (page === pageStart && page === pageEnd) {
+        buf = buf.slice(start - (page << PAGE_BITS), end - (page << PAGE_BITS) + 1)
+      } else if (page === pageStart) {
+        buf = buf.slice(start - (pageStart << PAGE_BITS), 1 << PAGE_BITS)
+      } else if (page === pageEnd) {
+        buf = buf.slice(0, end - (pageEnd << PAGE_BITS) + 1)
+      }
+      ret[page - pageStart] = buf
+    }
+    return ret
   }
 
-  // Return buffers
-  const ret = new Array(pageEnd - pageStart + 1)
-  for (let page = pageStart; page <= pageEnd; page++) {
-    let buf = await _pagePromiseCache[page]
-    if (page === pageStart && page === pageEnd) {
-      buf = buf.slice(start - (page << PAGE_BITS), end - (page << PAGE_BITS) + 1)
-    } else if (page === pageStart) {
-      buf = buf.slice(start - (pageStart << PAGE_BITS), 1 << PAGE_BITS)
-    } else if (page === pageEnd) {
-      buf = buf.slice(0, end - (pageEnd << PAGE_BITS) + 1)
-    }
-    ret[page - pageStart] = buf
+  async _readPage (page) {
+    console.log('loading page ' + page)
+
+    const start = page << PAGE_BITS
+    const end = ((page + 1) << PAGE_BITS) - 1
+    const headers = new Headers({
+      'Range': `bytes=${start}-${end}`
+    })
+
+    // TODO: use simple-get (which uses john's stream-http internally) to
+    // return a proper stream back, instead of this solution which waits for
+    // the full range request to return before returning the data
+    const res = await window.fetch(this._zipPath, { headers })
+    const abuf = await res.arrayBuffer()
+    const buf = Buffer.from(abuf)
+
+    console.log(`loaded ${start} to ${end}, ` +
+        `expected ${end - start + 1}b, got ${buf.length}b`)
+
+    return buf
   }
-  return ret
-}
-
-async function readPage (path, page) {
-  console.log('loading page ' + page)
-
-  const start = page << PAGE_BITS
-  const end = ((page + 1) << PAGE_BITS) - 1
-  const headers = new Headers({
-    'Range': `bytes=${start}-${end}`
-  })
-
-  // TODO: use simple-get (which uses john's stream-http internally) to
-  // return a proper stream back, instead of this solution which waits for
-  // the full range request to return before returning the data
-  const res = await window.fetch(path, { headers })
-  const abuf = await res.arrayBuffer()
-  const buf = Buffer.from(abuf)
-
-  console.log(`loaded ${start} to ${end}, ` +
-      `expected ${end - start + 1}b, got ${buf.length}b`)
-
-  return buf
 }
