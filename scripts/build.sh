2ac9fc2f254d368ee31d38250fabfff65a4ab399
DC <dcposch@dcpos.ch>
2018-01-20 03:18:07 -0500

    easier local development

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@ else
   mkdir -p build/$1
   cp -r static/* build/$1/
   cd build/$1
-  rm wiki.zip
+  rm -f wiki.zip list-full.json list-partial.json
   ln -s ../../transform/$1/wiki.zip ./
   ln -s ../../transform/$1/list-full.json ./
   ln -s ../../transform/$1/list-partial.json ./
