2296815cd3ab0a09409f295885daaa4a327d9a6a
Feross Aboukhadijeh <feross@feross.org>
2018-03-04 16:52:53 -0800

    build.sh: symlinks only

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,17 +3,16 @@ set -e
 
 npm run build
 
+rm -rf build/$1
 mkdir -p build/$1
-cp -r \
-  static/favicon.ico \
-  static/index.html \
-  static/bundle.js \
-  static/sphere.gif \
-  static/style.css \
-  build/$1/
-
 cd build/$1
-rm -f wiki.zip list-full.json list-partial.json
+
+ln -s ../../static/favicon.ico ./
+ln -s ../../static/index.html ./
+ln -s ../../static/bundle.js ./
+ln -s ../../static/sphere.gif ./
+ln -s ../../static/style.css ./
+
 ln -s ../../transform/$1/wiki.zip ./
 ln -s ../../transform/$1/list-full.json ./
 ln -s ../../transform/$1/list-partial.json ./
